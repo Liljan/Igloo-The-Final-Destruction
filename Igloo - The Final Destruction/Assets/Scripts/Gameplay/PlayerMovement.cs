c851cf3021d197b74d3a1c2ccd765fd504fdccf4
Liljan <marcus.t.lilja@gmail.com>
2017-08-31 13:53:06 +0200

    basic movement.

--- Igloo - The Final Destruction/Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speed = 1.0f;
+
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    public void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Movement();
 	}
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(speed * x, rb2d.velocity.y);
+    }
+
+    private void Jump()
+    {
+
+    }
 }
