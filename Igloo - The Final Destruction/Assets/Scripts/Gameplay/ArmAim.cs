6811a46e13f21fc94adfa99ed08a5acd9060941a
Liljan <marcus.t.lilja@gmail.com>
2017-09-05 14:16:51 +0200

    Free rotation.

--- Igloo - The Final Destruction/Assets/Scripts/Gameplay/ArmAim.cs
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/ArmAim.cs
@@ -4,31 +4,9 @@ using UnityEngine;
 
 public class ArmAim : MonoBehaviour
 {
-    public float sensitivity = 0.7f;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        float y = Input.GetAxis("Vertical");
-        Debug.Log(y);
-
-        if (y > sensitivity)
-        {
-            transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
-        }
-        else if (y < -sensitivity)
-        {
-            transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
-        }
-        else
-        {
-            transform.localRotation =  Quaternion.Euler(0.0f, 0.0f, 0.0f);
-        }
+        transform.localRotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Asin(y) * Mathf.Rad2Deg);
     }
 }
