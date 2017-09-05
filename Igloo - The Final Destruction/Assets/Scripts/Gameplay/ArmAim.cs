e6527632541d19f4fae7f6645eec114af0a46233
Liljan <marcus.t.lilja@gmail.com>
2017-09-05 14:06:55 +0200

    Aim.

---
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/ArmAim.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmAim : MonoBehaviour
+{
+    public float sensitivity = 0.7f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Input.GetAxis("Vertical");
+        Debug.Log(y);
+
+        if (y > sensitivity)
+        {
+            transform.localRotation = Quaternion.Euler(0.0f, 0.0f, 90.0f);
+        }
+        else if (y < -sensitivity)
+        {
+            transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -90.0f);
+        }
+        else
+        {
+            transform.localRotation =  Quaternion.Euler(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
---
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/WeaponSystem.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSystem : MonoBehaviour
+{
+
+    public List<GameObject> startingWeapons;
+    private List<GameObject> weapons;
+
+    private int activeWeapon = 0;
+    private bool dpadIsPressedDown = false;
+
+    private void Awake()
+    {
+        weapons = new List<GameObject>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        for (int i = 0; i < startingWeapons.Count; i++)
+        {
+            GameObject go = Instantiate(startingWeapons[i], transform.position, transform.rotation, this.transform);
+            go.SetActive(false);
+            weapons.Add(go);
+        }
+        weapons[0].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleWeaponChange();
+    }
+
+    private void HandleWeaponChange()
+    {
+        float dpadX = Input.GetAxis("TOGGLE_WEAPON_X");
+
+        if (dpadX == 0.0f)
+        {
+            dpadIsPressedDown = false;
+            return;
+        }
+
+        if (dpadIsPressedDown)
+            return;
+
+        if (dpadX == 1.0f)
+            ToggleWeaponForward();
+        else if (dpadX == -1.0f)
+            ToggleWeaponBackward();
+
+        dpadIsPressedDown = true;
+    }
+
+    private void ToggleWeaponForward()
+    {
+        weapons[activeWeapon].SetActive(false);
+
+        if (activeWeapon < weapons.Count - 1)
+        {
+            activeWeapon++;
+        }
+        else
+        {
+            activeWeapon = 0;
+        }
+
+        weapons[activeWeapon].SetActive(true);
+    }
+
+    private void ToggleWeaponBackward()
+    {
+        weapons[activeWeapon].SetActive(false);
+
+        if (activeWeapon > 0)
+        {
+            activeWeapon--;
+        }
+        else
+        {
+            activeWeapon = weapons.Count - 1;
+        }
+
+        weapons[activeWeapon].SetActive(true);
+    }
+}
