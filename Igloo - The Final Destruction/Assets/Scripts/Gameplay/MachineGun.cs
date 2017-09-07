50dea70bc6fc4cd25df2250bd78de9f439c7e62a
Liljan <marcus.t.lilja@gmail.com>
2017-09-07 12:43:24 +0200

    Machine gun.

--- Igloo - The Final Destruction/Assets/Scripts/Gameplay/MachineGun.cs
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/MachineGun.cs
@@ -3,22 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MachineGun : RangedWeapon {
-
+public class MachineGun : RangedWeapon
+{
     public float fireRate;
     private float fireTime;
-    private float fireTimer = 0.0f;
+    private float currentFireTime;
 
     public override void Initialize()
     {
         ammoInClip = clipSize;
 
         fireTime = 1F / fireRate;
+        currentFireTime = fireTime;
     }
 
     public override void PollShoot()
     {
-        throw new NotImplementedException();
+        currentFireTime += Time.deltaTime;
+
+        if (ammoInClip == 0)
+            return;
+
+        if (Input.GetButton("Fire") && currentFireTime >= fireTime)
+        {
+            SpawnShot();
+            ammoInClip--;
+
+            currentFireTime = 0.0f;
+        }
     }
 
 }
