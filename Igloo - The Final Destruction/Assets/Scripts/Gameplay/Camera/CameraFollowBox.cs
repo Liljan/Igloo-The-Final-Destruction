6720d72cb1f94008c7e486fc469d2f44ed93ea3b
Liljan <marcus.t.lilja@gmail.com>
2017-09-14 11:37:10 +0200

    Camera follows player with box.

---
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/Camera/CameraFollowBox.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowBox : MonoBehaviour
+{
+    public Vector2 margin;
+    public Vector2 smoothness;
+    //public Vector2 maxXY;        // The maximum x and y coordinates the camera can have.
+    //public Vector2 minXY;        // The minimum x and y coordinates the camera can have.
+    public Vector2 offset;
+
+    private Camera cam;
+
+    // public Vector2 offset;
+
+    public Transform player;        // Reference to the player's transform.
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!player)
+            return;
+
+        Vector2 target = new Vector2(transform.position.x, transform.position.y);
+
+        if (IsOutsideOfXMargin())
+            target.x = Mathf.Lerp(transform.position.x, player.position.x, smoothness.x * Time.deltaTime);
+
+        if (isOutsideOfYMargin())
+            target.y = Mathf.Lerp(transform.position.y, player.position.y, smoothness.y * Time.deltaTime);
+
+        transform.position = new Vector3(target.x, target.y, transform.position.z);
+    }
+
+    public void SetTarget(Transform t)
+    {
+        player = t;
+    }
+
+    private bool IsOutsideOfXMargin()
+    {
+        // Returns true if the distance between the camera and the player in the x axis is greater than the x margin.
+        Debug.Log(Mathf.Abs(transform.position.x - player.position.x));
+        return Mathf.Abs(transform.position.x - player.position.x) > margin.x;
+    }
+
+    private bool isOutsideOfYMargin()
+    {
+        // Returns true if the distance between the camera and the player in the y axis is greater than the y margin.
+        return Mathf.Abs(transform.position.y - player.position.y) > margin.y;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!player)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(2F * margin.x, 2F * margin.y, 1F));
+    }
+}
