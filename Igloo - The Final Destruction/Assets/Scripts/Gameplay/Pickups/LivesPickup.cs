a39f716f2c221485ba87d89870f3593c27f8e172
Liljan <marcus.t.lilja@gmail.com>
2017-09-29 23:03:38 +0200

    Extra life pickup.

--- Igloo - The Final Destruction/Assets/Scripts/Gameplay/Pickups/LivesPickup.cs
+++ Igloo - The Final Destruction/Assets/Scripts/Gameplay/Pickups/LivesPickup.cs
@@ -2,18 +2,11 @@
 
 public class LivesPickup : MonoBehaviour
 {
-    private LevelManager levelManager;
-
-    private void Awake()
-    {
-        levelManager = GameObject.FindObjectOfType<LevelManager>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            levelManager.AddLife();
+            LevelManager.Instance().AddLife();
             Destroy(gameObject);
         }
     }
